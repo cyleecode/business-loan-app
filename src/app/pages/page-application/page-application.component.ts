a908707e9c93be738abb28f8c59aad5b05e5bc3e
cyleecode <chunyitlee@gmail.com>
2023-04-17 21:56:32 +0800

    Page application OnInit -> applicationId

--- src/app/pages/page-application/page-application.component.ts
+++ src/app/pages/page-application/page-application.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-page-application',
   templateUrl: './page-application.component.html',
-  styleUrls: ['./page-application.component.scss']
+  styleUrls: ['./page-application.component.scss'],
 })
-export class PageApplicationComponent {
-
+export class PageApplicationComponent implements OnInit {
+  applicationId!: string | null;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.applicationId = this.route.snapshot.paramMap.get('appid');
+  }
 }
