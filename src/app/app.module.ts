c39620001ffe6ac516431682ac58a42d55a0b40b
cyleecode <chunyitlee@gmail.com>
2023-04-17 20:42:17 +0800

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { PageLandingComponent } from './pages/page-landing/page-landing.componen
 import { PageApplicationComponent } from './pages/page-application/page-application.component';
 import { PageOutcomeComponent } from './pages/page-outcome/page-outcome.component';
 import { DialogComponent } from './components/dialog/dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, PageLandingComponent, PageApplicationComponent, PageOutcomeComponent, DialogComponent],
-  imports: [BrowserModule, AppRoutingModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
