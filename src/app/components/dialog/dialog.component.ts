117866cdf6bded981f83c0a017e3f61cbe8af414
cyleecode <chunyitlee@gmail.com>
2023-04-20 21:28:52 +0800

    Dialog Component

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface IDialog {
+  componentPortal: ComponentPortal<any>;
+}
+
+export interface IDialogAction {
+  action: 'ok' | 'cancel' | 'submit';
+  data?: {};
+}
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.scss']
+  styleUrls: ['./dialog.component.scss'],
 })
 export class DialogComponent {
+  componentPortal!: ComponentPortal<any>;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: IDialog,
+    private dialog: MatDialogRef<DialogComponent, IDialogAction>
+  ) {
+    this.componentPortal = data.componentPortal;
+  }
 
+  close() {
+    this.dialog.close({
+      action: 'cancel',
+    });
+  }
 }
