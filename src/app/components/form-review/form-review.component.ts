bb3a479ddfb8b1153889fb6944e4212b98010e8f
cyleecode <chunyitlee@gmail.com>
2023-04-21 16:55:21 +0800

    UI & Fn: Update review form

--- src/app/components/form-review/form-review.component.ts
+++ src/app/components/form-review/form-review.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ISubmitForm } from 'src/app/interfaces/isubmit-form';
 
@@ -7,19 +7,20 @@ import { ISubmitForm } from 'src/app/interfaces/isubmit-form';
   templateUrl: './form-review.component.html',
   styleUrls: ['./form-review.component.scss'],
 })
-export class FormReviewComponent implements AfterViewInit {
+export class FormReviewComponent {
   form: ISubmitForm = {} as ISubmitForm;
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: ISubmitForm,
-    private dialog: MatDialogRef<FormReviewComponent>
+    private dialog: MatDialogRef<FormReviewComponent>,
   ) {
     this.form = data;
-  }
-  ngAfterViewInit(): void {
-    console.log(this.form);
   }
 
   isPositive(n: number) {
     return n > 0;
+  }
+
+  submit() {
+    this.dialog.close(true);
   }
 }
